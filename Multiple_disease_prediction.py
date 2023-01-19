5c752addd0828cb475ead91767650d538035f2e1
hansraj2000 <74312603+hansraj2000@users.noreply.github.com>
2023-01-19 18:46:11 +0530

    Update Multiple_disease_prediction.py

--- Multiple_disease_prediction.py
+++ Multiple_disease_prediction.py
@@ -59,8 +59,8 @@ if selected == 'Diabetes Prediction':
     
     with col1:
         DiabetesPedigramFunction = st.text_input("Diabetes Pedigree Function Value")
-#     with col2:
-#         Age = st.text_input("Age of the Person")
+    with col2:
+        Age = st.text_input("Age of the Person")
     
     
     
